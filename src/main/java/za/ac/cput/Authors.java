821a62aae9a68ff40d43d002930f6447801306bb
LTDO <lungatshila10@gmail.com>
2021-04-01 18:06:49 +0200

    lunga

---
+++ src/main/java/za/ac/cput/Authors.java
@@ -0,0 +1,62 @@
+/**
+ * Authors.java
+ * This is a concrete class for the Authors object
+ * @author Lunga Tshila
+ * Date: Tuesday, 30 March 2021
+ *
+ */
+
+package za.ac.cput;
+
+public class Authors {
+
+
+    private final String title, author;
+
+    public Authors(String title, String author) {
+        if (title == null || author == null)
+            throw new NullPointerException();
+
+        this.title = title;
+        this.author = author;
+    }
+
+
+    public String title() {
+        return title;
+    }
+
+    public String author() {
+        return author;
+    }
+
+    //For ObjectIdentity
+    @Override
+    public boolean equals(Object other) {
+        if (this == other)
+            return true;
+        if (!(other instanceof Authors))
+            return false;
+
+        Authors book = (Authors) other;
+        return title.equals(book.title) &&
+                author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return title.hashCode() + author.hashCode() * 7;
+    }
+
+    @Override
+    public String toString() {
+        return "\"" + title + "\" by " + author;
+    }
+
+    //For Failing Test
+    public void method(int x) throws IllegalArgumentException {
+        if (x < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/test/java/za/ac/cput/AuthorsTest.java
@@ -0,0 +1,59 @@
+/**
+ * AuthorsTest.java
+ * This is a test class for Authors object
+ * @author Lunga Tshila
+ * Date: Tuesday, 30 March 2021
+ *
+ */
+
+package za.ac.cput;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorsTest {
+
+    Authors fail;
+    @Test
+    public void testBookIdentity(){
+        Authors book = new Authors("The Art of Hustling", "Dj SBU");
+        Authors book2 = new Authors("The Art of Trading", "Dr Ref Wayne");
+        Authors book3 = new Authors("Crime and Punishment", "Dr Nietche Djoviesky");
+        Authors book4 = book3;
+
+        assertEquals(book.equals(book2), false);
+        assertEquals(book2.equals(book3), false);
+        assertEquals(book3.equals(book4), true);
+    }
+
+    @Test
+    public void testBookIdentityUsingHashcode(){
+        Authors book = new Authors("The Art of Hustling", "Dj SBU");
+        Authors book2 = new Authors("The Art of Trading", "Dr Ref Wayne");
+        Authors book3 = new Authors("Crime and Punishment", "Dr Nietche Djoviesky");
+        Authors book4 = book3;
+
+        assertEquals(book.hashCode(), -1117918411);
+        assertEquals(book2.hashCode(), -178128203);
+
+        //book4 and book3 point to the same object therefore their hashcode is identical
+        assertEquals(book3.hashCode(), -668276722);
+        assertEquals(book4.hashCode(), -668276722);
+    }
+
+
+    @Test
+    public void failExampleTest() {
+        if(fail == null){
+            Assertions.fail("fail is null");
+        }
+        try {
+            fail.method(-1);
+            Assertions.fail("Should of thrown an IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            e.getMessage();
+        }
+    }
+}
